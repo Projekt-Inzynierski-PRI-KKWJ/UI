648a77eb2bafa1514ca40865a4546aee7974db05
jakub <jakub.czaja@icloud.com>
2025-10-29 22:21:02 +0100

    allow project edits by supervisors

--- src/app/modules/project/components/project-details/project-details.component.ts
+++ src/app/modules/project/components/project-details/project-details.component.ts
@@ -423,6 +423,10 @@ export class ProjectDetailsComponent implements OnInit, OnDestroy {
       )
       ||
       (this.user.role === 'COORDINATOR')
+      ||
+      (this.user.role === 'SUPERVISOR' && 
+       this.user.acceptedProjects.includes(this.data.id!)
+      )
     ){
       return true
     } else {
