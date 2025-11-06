4f420e3758cdbd4afa98af1562136486e5b9d2b7
Krzysztof Rożek <seriousemail468@gmail.com>
2025-11-06 21:35:47 +0100

    Fix for broken relative paths - prof. Zywica couldn't use the app

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+const base = document.querySelector('base');
+
+if (base) 
+{
+  const path = window.location.pathname.split('/')[1];
+
+  if (path && path !== '') 
+  {
+    base.setAttribute('href', `/${path}/`);
+  }
+  
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
