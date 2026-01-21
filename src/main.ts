e2a06d8e7ddeab25d697d9e28935caed85d00694
Krzysztof Rożek <seriousemail468@gmail.com>
2026-01-22 00:58:41 +0100

    Documentation for adding project from market, automating href paths

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,29 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
+// This needs to stay here, because it allows for dynamic configuration of paths 
+const base = document.querySelector('base');
+
+if (base) {
+  const isLocalhost =
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '127.0.0.1';
+
+  if (isLocalhost) 
+  {
+    base.setAttribute('href', '/');
+  } 
+  else 
+  {
+    const segments = window.location.pathname.split('/').filter(Boolean);
+
+    if (segments.length > 0) 
+    {
+      base.setAttribute('href', `/${segments[0]}/`);
+    }
+  }
+}
+// Do not delete this fragment of code above, between the comments 
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
